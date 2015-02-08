c84d486b98388e4134bd6dc5ec09b988f074da07
Nils Eriksson <niler851@student.liu.se>
2015-02-08 18:44:20 +0100

    add write to server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,15 +4,33 @@ var index = angular.module("index", []);
 
 contactlist.controller("TableController", function ($scope, $http) {
 
-	$http.get('/contactlist').success(function(res){
-		console.log("data requested sent back");
-		$scope.contactlist = res;
-	});
+	var refresh = function(){
+		$http.get('/contactlist').success(function(res){
+			console	.log("data requested sent back");
+			$scope.contactlist = res;
+		});
+	};
+	refresh();
+
+	$scope.removeContact = function(id){
+		console.log(id);
+		$http.delete('/contactlist/'+id).success(function(res){
+			console.log("removed " + res);
+			refresh();
+		});
+	}
+
 
 });
 
-index.controller("IndexController", function($scope){
-	console.log("herp derp index");
+index.controller("IndexController", function($scope,$http){
+	$scope.addContact = function(){
+		console.log($scope.index.contact);
+		$http.post('/contactlist', $scope.index.contact).success(function(res){
+			console.log("Contact added to database");
+			console.log(res);
+		});
+	};
 });
 
 })();
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var app = express();
 var mongojs = require("mongojs");
 //database and collection
 var db = mongojs('contactlist',['contactlist']);
-
+var bodyParser =  require('body-parser');
 
 app.use(express.static(__dirname +"/public"));
+app.use(bodyParser.json());
+
 
 app.get('/contactlist', function(req, res){
 	console.log("GET request");
@@ -15,6 +17,21 @@ app.get('/contactlist', function(req, res){
 	});
 });
 
+app.post('/contactlist',function(req,res){
+	console.log(req.body);
+	db.contactlist.insert(req.body, function( err, doc){
+		res.json(doc);
+	});
+});
+
+app.delete('/contactlist/:id',function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+
+});
 
 app.listen(3000);
 console.log("server running on port 3000");
